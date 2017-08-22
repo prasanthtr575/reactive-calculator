0d7eee0c84c2c07ba44656b5d5de4ff98968eec5
Prasanth T R <mailprasanthtr@gmail.com>
2017-08-22 08:00:45 +0530

    Assemble React + Redux

---
+++ src/js/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () => {
+	return (
+		<div>
+			<nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
+  				<h2 className="navbar-brand">Reactive Calculator</h2>
+			</nav>
+		</div>
+	)
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,116 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import { User } from './components/User';
+import { Main } from './components/Main';
+import { Header } from './components/Header';
+
 class App extends React.Component {
-	render() {
-		return (
-			<div>
-				<h1>Reactive Calculator</h1>
-			</div>
-		);
-	}
+	constructor() {
+        super();
+        this.state = {
+            username: "Max"
+        };
+    }
+
+    changeUsername(newName) {
+        this.setState({
+            username: newName
+        });
+    }
+
+    render() {
+        return (
+            <div className="container">
+            	<Header/>
+                <Main changeUsername={this.changeUsername.bind(this)}/>
+                <User username={this.state.username}/>
+            </div>
+        );
+    }
 }
 
 render(<App/>, window.document.getElementById("reactCalApp"));
+
+/*
+import {render} from "react-dom";
+import React from "react";
+import {createStore, combineReducers, applyMiddleware} from "redux";
+import { createLogger } from 'redux-logger'
+
+const mathReducer = (state = {
+    result: 1,
+    lastValues: []
+}, action) => {
+    switch (action.type) {
+        case "ADD":
+            state = {
+                ...state,
+                result: state.result + action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            };
+            break;
+        case "SUBTRACT":
+            state = {
+                ...state,
+                result: state.result - action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            };
+            break;
+    }
+    return state;
+};
+
+const userReducer = (state = {
+    name: "Max",
+    age: 27
+}, action) => {
+    switch (action.type) {
+        case "SET_NAME":
+            state = {
+                ...state,
+                name: action.payload
+            };
+            break;
+        case "SET_AGE":
+            state = {
+                ...state,
+                age: action.payload
+            };
+            break;
+    }
+    return state;
+};
+
+const myLogger = (state) => (next) => (action) => {
+    // console.log("Logged Action: ", action);
+    next(action);
+};
+
+const store = createStore(
+    combineReducers({mathReducer, userReducer}),
+    {},
+    applyMiddleware(myLogger, createLogger())
+);
+
+store.subscribe(() => {
+    // console.log("Store updated!", store.getState());
+});
+
+store.dispatch({
+    type: "ADD",
+    payload: 100
+});
+store.dispatch({
+    type: "ADD",
+    payload: 22
+});
+store.dispatch({
+    type: "SUBTRACT",
+    payload: 80
+});
+store.dispatch({
+    type: "SET_AGE",
+    payload: 30
+});
+*/
